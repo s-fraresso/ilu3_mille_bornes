75d0e1f25d1601fade33863db9a0d6b62fe729a5
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2026-02-19 09:37:20 +0100

    iterateur

---
+++ src/jeu/Sabot.java
@@ -0,0 +1,87 @@
+package jeu;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import cartes.Carte;
+
+public class Sabot implements Iterable<Carte> {
+	private Carte[] cartes;
+	private int nbCartes;
+	private int nombreOperations = 0;
+
+	public Sabot(Carte[] cartes) {
+		this.cartes = cartes;
+		nbCartes = cartes.length;
+	}
+	
+	public boolean estVide() {
+		return nbCartes == 0;
+	}
+	
+	public void ajouterCarte(Carte carte) {
+		nombreOperations++;
+		cartes[nbCartes++] = carte;
+	}
+	
+	public Carte piocher() {
+		Iterator<Carte> iterateur = iterator();
+		Carte sommet = iterateur.next();
+		iterateur.remove();
+		return sommet;
+	}
+
+	@Override
+	public Iterator<Carte> iterator() {
+		return new Iterateur();
+	}
+	
+	private class Iterateur implements Iterator<Carte>{
+		private int indiceIterateur = 0;
+		private boolean nextEffectue = false;
+		private int nombreOperationsReference = nombreOperations;
+		
+		@Override
+		public boolean hasNext() {
+			return indiceIterateur < nbCartes;
+		}
+
+		@Override
+		public Carte next() {
+			verificationConcurrence();
+			
+			if (hasNext()) {
+				Carte carte = cartes[indiceIterateur++];
+				nextEffectue = true;
+				return carte;
+			}
+			else {
+				throw new NoSuchElementException();
+			}
+		}
+		
+		@Override
+		public void remove() {
+			verificationConcurrence();
+			
+			if (nbCartes < 1 || !nextEffectue) {
+				throw new IllegalStateException();
+			}
+			for (int i = indiceIterateur - 1; i < cartes.length - 1; i++) {
+				cartes[i] = cartes[i + 1];
+			}
+			nextEffectue = false;
+			indiceIterateur--;
+			nbCartes--;
+		}
+		
+		private void verificationConcurrence() {
+			if (nombreOperations != nombreOperationsReference) {
+				throw new ConcurrentModificationException();
+			}
+		}
+		
+	}
+	
+}

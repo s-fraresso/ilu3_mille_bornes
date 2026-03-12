55cc75d6acf847dd2a3143b24aea7dfa6a0d6518
unknown <sylvainfraresso@laposte.net>
2026-03-12 18:21:13 +0100

    tests gestion cartes

---
+++ src/testsFonctionnels/TestGestionCartes.java
@@ -0,0 +1,30 @@
+package testsFonctionnels;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import cartes.Carte;
+import cartes.JeuDeCartes;
+import utils.GestionCartes;
+
+public class TestGestionCartes {
+	public static void main(String args[]) {
+		JeuDeCartes jeu = new JeuDeCartes();
+		List<Carte> listeCarteNonMelangee = new LinkedList<>();
+		for (Carte carte : jeu.donnerCartes()) {
+			listeCarteNonMelangee.add(carte);
+		}
+		List<Carte> listeCartes = new ArrayList<>(listeCarteNonMelangee);
+		System.out.println(listeCartes);
+		listeCartes = GestionCartes.melanger(listeCartes);
+		System.out.println(listeCartes);
+		System.out.println(
+				"liste m�lang�e sans erreur ? " + GestionCartes.verifierMelange(listeCarteNonMelangee, listeCartes));
+		listeCartes = GestionCartes.rassembler(listeCartes);
+		System.out.println(listeCartes);
+		System.out.println("liste rassembl�e sans erreur ? " + GestionCartes.verifierRassemblement(listeCartes));
+
+	}
+
+}
--- src/testsFonctionnels/TestMethodeEquals.java
+++ src/testsFonctionnels/TestMethodeEquals.java
@@ -2,24 +2,21 @@ package testsFonctionnels;
 
 import cartes.Attaque;
 import cartes.Borne;
-import cartes.Type;
 import cartes.Carte;
 import cartes.Parade;
-
+import cartes.Type;
 
 public class TestMethodeEquals {
-	
 	public static void main(String[] args) {
-		Carte borne1 = new Borne(25);
-		Carte borne2 = new Borne(25);
+		Carte carte25Bornes1 = new Borne(25);
+		Carte carte25Bornes2 = new Borne(25);
+		System.out.println("Deux cartes de 25km sont identiques ? " + carte25Bornes1.equals(carte25Bornes2));
+	
+		Carte carteFeuxRouge1 = new Attaque(Type.FEU);
+		Carte carteFeuxRouge2 = new Attaque(Type.FEU);
+		System.out.println("Deux cartes de feux rouge sont identiques ? " + carteFeuxRouge1.equals(carteFeuxRouge2));
 		
-		Carte feuRouge1 = new Attaque(Type.FEU);
-		Carte feuRouge2 = new Attaque(Type.FEU);
-		
-		Carte feuVert = new Parade(Type.FEU);
-		
-		System.out.println("Deux cartes de 25km sont identiques ?" + borne1.equals(borne2));
-		System.out.println("Deux cartes de feux rouge sont identiques ?" + feuRouge1.equals(feuRouge2));
-		System.out.println("La carte feu rouge et la carte feu vert sont identiques ?" + feuRouge1.equals(feuVert));
+		Carte carteFeuxVert = new Parade(Type.FEU);
+		System.out.println("La carte feu rouge et la carte feu vert sont identiques ? " + carteFeuxRouge1.equals(carteFeuxVert));
 	}
 }

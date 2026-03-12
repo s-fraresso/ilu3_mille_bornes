8ef500fe9a52661e30706fd219bf11ef297d6954
unknown <sylvainfraresso@laposte.net>
2026-03-12 18:28:21 +0100

    classe jeu

---
+++ src/jeu/Jeu.java
@@ -0,0 +1,22 @@
+package jeu;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import cartes.Carte;
+import cartes.JeuDeCartes;
+import utils.GestionCartes;
+
+public class Jeu {
+	private Sabot sabot;
+	
+	public Jeu() {
+		JeuDeCartes jeuDeCartes = new JeuDeCartes();
+		Carte[] cartes = jeuDeCartes.donnerCartes();
+		List<Carte> listeCartes = new ArrayList<>();
+		Collections.addAll(listeCartes, cartes);
+		listeCartes = GestionCartes.melanger(listeCartes);
+		sabot = new Sabot(listeCartes.toArray(new Carte[0]));
+	}
+}

0291959995152461fe8e53f298d2244eeeb0a902
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2026-03-26 09:25:36 +0100

    test zone de jeu

--- src/cartes/Cartes.java
+++ src/cartes/Cartes.java
@@ -1,7 +1,7 @@
 package cartes;
 
 public interface Cartes {
-	public Carte PRIORITAIRE = new Botte(Type.FEU); 
-	public Carte FEU_ROUGE = new Attaque(Type.FEU); 
-	public Carte FEU_VERT = new Parade(Type.FEU); 
+	public final Carte PRIORITAIRE = new Botte(Type.FEU); 
+	public final Carte FEU_ROUGE = new Attaque(Type.FEU); 
+	public final Carte FEU_VERT = new Parade(Type.FEU); 
 }

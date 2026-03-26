7f717b52e3eb2435179bce5b4ede4c0d29ea2139
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2026-03-26 08:01:45 +0100

    piles zone de jeu

---
+++ src/jeu/ZoneDeJeu.java
@@ -0,0 +1,15 @@
+package jeu;
+
+import java.util.List;
+
+import cartes.Bataille;
+import cartes.Borne;
+import cartes.Carte;
+
+public class ZoneDeJeu {
+	List<Bataille> pileBataille;
+	List<Carte> pileLimite;
+	List<Borne> pileBornes;
+	
+	
+}

f3436e2a6ab4ad142d8b60403a518f32bdfc9bd0
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2026-04-23 09:12:41 +0200

    interface Strategie, trierCoups

---
+++ src/strategies/Strategie.java
@@ -0,0 +1,30 @@
+package strategies;
+
+import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+import jeu.Coup;
+
+public interface Strategie {
+	public default NavigableSet<Coup> trierCoups(Set<Coup> coups){
+		NavigableSet<Coup> coupsTries = new TreeSet<>(
+			new Comparator<Coup>() {
+				private Random random = new Random();
+				
+				@Override
+				public int compare(Coup coup1, Coup coup2) {
+					if (coup1.equals(coup2)) {
+						return 0;
+					}
+					
+					return random.nextBoolean() ? 1 :  -1;
+				}
+			}
+		);
+		
+		return coupsTries;
+	}
+}
